ae4b9f98bd9cc35edf8e4b352f93f7973223ecb4
s2100932 <s2100932@usls.edu.ph>
2026-08-14 21:52:24 +0800

    Part 1.4: InsufficientBatteryError with battery required and amount available

--- main.py
+++ main.py
@@ -29,7 +29,10 @@ class Robot(abc.ABC):
     def perform_task(self):
         pass
         
-
+    def batConsumption(self, batCharge):
+        if batCharge > self.battery:
+            raise InsufficientBatteryError(self.name, batCharge, self.battery)
+        self.battery -= batCharge
 
 class CleaningRobot(Robot):
     def __str__(self):
@@ -40,8 +43,9 @@ class CleaningRobot(Robot):
         self.dustbin = dustbin
 
     def perform_task(self):
+        self.batConsumption(5)
         print(f"{self.name} is cleaning the floor and has a {self.dustbin}% full dustbin.")
-        self.battery -= 5
+
 
 
 
@@ -54,13 +58,27 @@ class DroneRobot(Robot):
         self.camera_quality = camera_quality
 
     def perform_task(self):
+        self.batConsumption(10)
         print(f"{self.name} is flying and taking aerial photos with {self.camera_quality} camera.")
-        self.battery -= 12
+        
 
 
 def fleet_report(robots):
+    print()
+    print(f"Fleet Created: {Robot.botMany} Robots")
     for r in robots:
         print(str(r))
+    print()
 
 fleet = [CleaningRobot("Roomba"), DroneRobot("HeliDrone")]
 fleet_report(fleet)
+
+class InsufficientBatteryError(Exception):
+    def __init__(self, name, required, available):
+        self.name = name
+        self.required = required
+        self.available = available
+        super().__init__(self._build_message())
+
+    def _build_message(self):
+        return f"{self.name} has insufficient battery. Required: {self.required}%, Available: {self.available}%"
